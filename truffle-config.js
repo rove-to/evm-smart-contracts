eaaacec13fad886831c5505a665b7be0708e2026
thach4567 <thach4567@gmail.com>
2022-01-12 10:25:36 +0700

    update truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -59,12 +59,12 @@ module.exports = {
     // Useful for deploying to a public network.
     // NB: It's important to wrap the provider as a function.
     bsctestnet: {
-      provider: () => new PrivateKeyProvider(process.env.PRIV_KEY, "https://data-seed-prebsc-2-s2.binance.org:8545/"),
+      provider: () => new PrivateKeyProvider(process.env.PRIV_KEY, "https://data-seed-prebsc-1-s1.binance.org:8545"),
       network_id: 97,       // Ropsten's id
       gasPrice: 1e10,        // Ropsten has a lower block limit than mainnet
       confirmations: 1,    // # of confs to wait between deployments. (default: 0)
-      // timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
-      // skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
+      timeoutBlocks: 200,
+      skipDryRun: true
     },
     // Useful for private networks
     // private: {
