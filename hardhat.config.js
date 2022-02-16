afcc2535ef6c3e5591cd64c1f47c1f8178f0d612
thaibao-incognito <thaibao@autonomous.nyc>
2022-02-16 10:08:33 +0700

    - hardhat config

---
+++ hardhat.config.js
@@ -0,0 +1,25 @@
+/**
+ * @type import('hardhat/config').HardhatUserConfig
+ */
+require("dotenv").config();
+require("@nomiclabs/hardhat-ethers");
+
+module.exports = {
+  solidity: "0.8.1",
+  defaultNetwork: "rinkeby",
+  networks: {
+    hardhat: {},
+    rinkeby: {
+      url: process.env.RINKEBY_DEV_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    ropsten: {
+      url: process.env.ROPSTEN_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    mainnet: {
+      url: process.env.MAINNET_API_URL,
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+  },
+};
