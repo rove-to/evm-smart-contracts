5900f0ea45904706f0f6090c7c1262adbfad76b6
thaibao-incognito <thaibao@autonomous.nyc>
2022-02-22 07:54:17 +0700

    - test object nft

--- test/object_nft_test.js
+++ test/object_nft_test.js
@@ -6,7 +6,11 @@ const expect = chai.expect;
 const {addresses} = require("./constants");
 const hardhatConfig = require("../hardhat.config");
 let nft_owner_address = '0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'; // default for local
-
+function sleep(second) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, second * 1000);
+    });
+}
 describe("** NFTs erc-1155 contract", function () {
     let objectNFT;
 
@@ -61,22 +65,38 @@ describe("** NFTs erc-1155 contract", function () {
             let tokenURI = "https://gateway.pinata.cloud/ipfs/QmWYZQzeTHDMGcsUMgdJ64hgLrXk8iZKDRmbxWha4xdbbH";
 
             // check token init
+            console.log("+ check token init");
             let newItemId = await objectNFT.newItemId()
             console.log("newItemId:", newItemId);
             expect(newItemId).to.equal(0);
 
-            // check token id minted
+            // check token id minted 1st
+            console.log("+ check token id minted 1st");
             await objectNFT.mintNFT(nftOwner, initSupply, tokenURI);
+            await sleep(10);
             let tokenID = await objectNFT.newItemId()
+            expect(tokenID).to.equal(1);
+            console.log("tokenID:", tokenID);
+
+            // check token id minted 2nd
+            console.log("+ check token id minted 2nd");
+            await objectNFT.mintNFT(nftOwner, initSupply, tokenURI);
+            await sleep(10);
+            tokenID = await objectNFT.newItemId();
+            expect(tokenID).to.equal(2);
             console.log("tokenID:", tokenID);
 
             // transfer and check balance
-            let tx = await objectNFT.safeTransferFrom(nftOwner, receiver, tokenID, 3, "0x");
+            console.log("+ transfer and check balance")
+            let tx = await objectNFT.safeTransferFrom(nftOwner, receiver, tokenID, 5, "0x");
             console.log("Transfer tx:", tx);
+            await sleep(10);
             let balance_erc1155_receiver = await objectNFT.balanceOf(receiver, tokenID);
             console.log("balance of receiver %s on token %s is %s", receiver, tokenID, balance_erc1155_receiver);
+            await sleep(10);
             let balance_erc1155_owner = await objectNFT.balanceOf(nftOwner, tokenID);
             console.log("balance of nft owner %s on token %s is %s", nftOwner, tokenID, balance_erc1155_owner);
+            await sleep(10);
             expect(balance_erc1155_receiver.add(balance_erc1155_owner)).to.equal(initSupply);
         });
     });
