853639435a0f9d7de99b1d7744b7dbe9331d2d4c
thaibao-incognito <thaibao@autonomous.nyc>
2022-02-24 08:03:13 +0700

    - update

--- config.ts
+++ config.ts
@@ -4,7 +4,7 @@ export default {
     PRIVATE_KEY: process.env.PRIVATE_KEY ?? '',
     NETWORK: process.env.NETWORK,
     POLYGON_MUMBAI_API_URL: process.env.POLYGON_MUMBAI_API_URL,
-    OBJECT_NFT_CONTRACT: '',
+    OBJECT_NFT_CONTRACT: '0x12e20e3E5122bB9455544814A0e7e3D9df610cAA',
 
     BE_URL_API: process.env.BE_URL_API ?? '',
 
--- scripts/objectNFT/2_mint.ts
+++ scripts/objectNFT/2_mint.ts
@@ -1,18 +1,17 @@
 import config from "../../config";
 import {ObjectNFT} from "./objectNFT";
-import {PinataIpfsStorage} from "../utils/pinata_upload";
+import {PinataIpfsStorage} from "../utils/pinataIpfsStorage";
 
 (async () => {
     try {
-        const objectNFT = new ObjectNFT(config.ACCESS_TOKEN, config.PRIVATE_KEY, config.PUBLIC_KEY);
-
-        var pinataIpfsStorage = new PinataIpfsStorage(config.ACCESS_TOKEN);
-        const pinatahashlink = await pinataIpfsStorage.uploadObjectNFT(
+        let pinata = new PinataIpfsStorage(config.ACCESS_TOKEN);
+        const pinatahashlink = await pinata.uploadObjectNFT(
             "./metadatajson/Test-Drive-Unlimited-new-2-icon.png",
             "./metadatajson/corgi.arc",
             './metadatajson/object_nft.json');
         console.log("result: ", pinatahashlink);
 
+        const objectNFT = new ObjectNFT(config.ACCESS_TOKEN, config.PRIVATE_KEY, config.PUBLIC_KEY);
         const tx = await objectNFT.mintObjectNFT(config.PUBLIC_KEY, config.OBJECT_NFT_CONTRACT, 1, pinatahashlink, 500000);
         console.log("tx hash: ", tx);
     } catch (e) {
