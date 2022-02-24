ce3883a28552e9bfab74c39b5f528647cb55a5f1
thaibao-incognito <thaibao@autonomous.nyc>
2022-02-24 08:02:58 +0700

    - update

--- scripts/objectNFT/1_deploy.ts
+++ scripts/objectNFT/1_deploy.ts
@@ -3,7 +3,7 @@ import {ObjectNFT} from "./objectNFT";
 
 (async () => {
     try {
-        const objectNFT = new ObjectNFT(config.ACCESS_TOKEN, config.PUBLIC_KEY, config.PRIVATE_KEY);
+        const objectNFT = new ObjectNFT(config.ACCESS_TOKEN, config.PRIVATE_KEY, config.PUBLIC_KEY);
         const address = await objectNFT.deploy();
         console.log("ObjectNFT deployed address: ", address);
     } catch (e) {
