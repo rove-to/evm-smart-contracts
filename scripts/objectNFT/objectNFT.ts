ed9e9cac37bb2734372fcca4d84066a91a1e9429
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-01 11:45:46 +0700

    - remove proxy

--- scripts/objectNFT/objectNFT.ts
+++ scripts/objectNFT/objectNFT.ts
@@ -17,15 +17,8 @@ class ObjectNFT {
     }
 
     async deploy() {
-        let proxyRegistryAddress = "";
-        if (this.network === 'rinkeby') {
-            proxyRegistryAddress = "0xf57b2c51ded3a29e6891aba85459d600256cf317";
-        } else {
-            proxyRegistryAddress = "0xa5409ec958c83c3f309868babaca7c86dcb077c1";
-        }
-
         const ObjectNFT = await ethers.getContractFactory("ObjectNFT");
-        const ObjectNFTDeploy = await ObjectNFT.deploy(proxyRegistryAddress);
+        const ObjectNFTDeploy = await ObjectNFT.deploy();
 
         console.log("Rove ObjectNFT deployed:", ObjectNFTDeploy.address);
         return ObjectNFTDeploy.address;
