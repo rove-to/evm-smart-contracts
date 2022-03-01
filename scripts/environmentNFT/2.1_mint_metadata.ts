d3246333fd0adfb99910b02b7f1d6d79e69e9056
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-01 14:46:48 +0700

    - update

--- scripts/environmentNFT/2.1_mint_metadata.ts
+++ scripts/environmentNFT/2.1_mint_metadata.ts
@@ -4,70 +4,56 @@ import {PinataIpfsStorage, PinataData} from "../utils/pinataIpfsStorage";
     try {
         // set image
         let image2DPath: string = "";
-        if (process.argv.length < 3) {
-            console.log("miss image");
-            return;
+        if (process.argv.length >= 2) {
+            image2DPath = process.argv[2];
         }
-        image2DPath = process.argv[2];
 
         // 
         let model3DPath: string = "";
-        if (process.argv.length < 4) {
-            console.log("miss 3d model");
-            return;
+        if (process.argv.length >= 3) {
+            model3DPath = process.argv[3];
         }
-        model3DPath = process.argv[3];
+
 
         //
         let assetBundlePath: string = "";
-        if (process.argv.length < 5) {
-            console.log("miss asset bundle");
-            return;
+        if (process.argv.length >= 4) {
+            assetBundlePath = process.argv[4];
         }
-        assetBundlePath = process.argv[4];
+
 
         //
         let assetAddressablePath: string = "";
-        if (process.argv.length < 6) {
-            console.log("miss asset addressable");
-            return;
+        if (process.argv.length >= 5) {
+            assetAddressablePath = process.argv[5];
         }
-        assetAddressablePath = process.argv[5];
+
 
         let jsonTemplate: string = './metadatajson/environment_nft.json';
-        if (process.argv.length < 7) {
-            console.log("miss json template");
-            return;
+        if (process.argv.length >= 6) {
+            jsonTemplate = process.argv[6];
         }
-        jsonTemplate = process.argv[6];
 
         // set name
         let name: string = '';
-        if (process.argv.length < 8) {
-            console.log("miss name");
-            return;
+        if (process.argv.length >= 7) {
+            name = process.argv[7];
         }
-        name = process.argv[7];
 
         // set description 
         let description: string = '';
-        if (process.argv.length < 9) {
-            console.log("miss description");
-            return;
+        if (process.argv.length >= 8) {
+            description = process.argv[8];
         }
-        description = process.argv[8];
+
         // set attributes
         let attributes: any;
-        if (process.argv.length < 10) {
-            console.log("miss attributes");
-            return;
+        if (process.argv.length >= 9) {
+            attributes = process.argv[9];
         }
-        attributes = process.argv[9];
 
         let data = new PinataData(image2DPath, model3DPath, assetBundlePath, assetAddressablePath, jsonTemplate, name, description, attributes)
         console.log(data);
-        const nftContract = process.env.ENVIRONMENT_NFT_CONTRACT;
-        console.log("nft contract", nftContract);
 
         let pinata = new PinataIpfsStorage(process.env.ACCESS_TOKEN);
         const tokenURI = await pinata.uploadObjectNFT(data);
--- scripts/environmentNFT/2.2_mint_nft.ts
+++ scripts/environmentNFT/2.2_mint_nft.ts
@@ -5,36 +5,38 @@ import {PinataIpfsStorage, PinataData} from "../utils/pinataIpfsStorage";
 
 (async () => {
     try {
+        console.log(process.argv);
+        
         // set init owner
         let initOwnerAddress: any;
-        if (process.argv.length < 3) {
-            console.log("miss init owner nft");
-            return;
+        if (process.argv.length >= 2) {
+            initOwnerAddress = process.argv[2];
         }
-        initOwnerAddress = process.argv[2];
         console.log("init owner:", initOwnerAddress);
 
+        // set init supply
         let initSupply: any;
-        if (process.argv.length < 4) {
-            console.log("miss init supply");
-            return;
+        if (process.argv.length >= 3) {
+            initSupply = process.argv[3];
         }
-        initSupply = process.argv[3];
         console.log("initSupply:", initSupply);
-        
+
         // set metadata
         let tokenURI: any;
-        if (process.argv.length < 5) {
-            console.log("miss init owner nft");
-            return;
+        if (process.argv.length >= 4) {
+            tokenURI = process.argv[4];
         }
-        initOwnerAddress = process.argv[4];
-        console.log("tokenUri:", initOwnerAddress);
-        
-        let nftContract:any = process.env.ENVIRONMENT_NFT_CONTRACT;
-        
-        const objectNFT = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const tx = await objectNFT.mintEnvironmentNFT(initOwnerAddress, nftContract, initSupply, tokenURI, 500000);
+        console.log("tokenUri:", tokenURI);
+
+        let nftContract: any;
+        nftContract = process.env.ENVIRONMENT_NFT_CONTRACT;
+        if (process.argv.length > 5) {
+            nftContract = process.argv[5];
+        }
+        console.log("nftContract:", nftContract);
+
+        const nft = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const tx = await nft.mintEnvironmentNFT(initOwnerAddress, nftContract, initSupply, tokenURI, 500000);
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
