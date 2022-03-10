fa933dfccb1b4b5f6ccb23a91c9a6f09648ade69
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-10 09:55:59 +0700

    solidity 0.8.12

--- contracts/utils/ERC721Tradable.sol
+++ contracts/utils/ERC721Tradable.sol
@@ -1,4 +1,4 @@
-pragma solidity ^0.8.0;
+pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/token/ERC721/presets/ERC721PresetMinterPauserAutoId.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
