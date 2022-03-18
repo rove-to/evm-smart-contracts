c7bc72d1d62a5ebcfee5261b1da1b353d6953c43
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-18 09:53:20 +0700

    deploy

--- migrations/environmentNFT/1_deploy.ts
+++ migrations/environmentNFT/1_deploy.ts
@@ -4,8 +4,12 @@ import {EnvironmentNFT} from "./environmentNFT";
 
 (async () => {
     try {
+        if (process.env.NETWORK != "local") {
+            console.log("wrong network");
+            return;
+        }
         const nft = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await nft.deploy();
+        const address = await nft.deploy('', '');
         console.log("%s EnvironmentNFT deployed address: %s", process.env.NETWORK, address);
     } catch (e) {
         // Deal with the fact the chain failed
