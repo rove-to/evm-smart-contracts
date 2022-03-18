2c81cc3372a756859393f5ac41f43b8da3c489d8
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-18 09:52:38 +0700

    - get admin script

--- migrations/environmentNFT/1_getadmin.ts
+++ migrations/environmentNFT/1_getadmin.ts
@@ -4,13 +4,13 @@ import {EnvironmentNFT} from "./environmentNFT";
 
 (async () => {
     try {
-        if (process.env.NETWORK != "polygon") {
+        if (process.env.NETWORK != "local") {
             console.log("wrong network");
             return;
         }
         const nft = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await nft.getAdminAddress('');
-        console.log("%s Environment admin address: %s", process.env.NETWORK, address);
+        let admin, operator = await nft.getAdminAddress('');
+        console.log("%s Environment admin address: %s", process.env.NETWORK, admin, operator);
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
