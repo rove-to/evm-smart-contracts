34c9103cf172edf3dbad287a4959c0acfea6b22e
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-23 21:01:41 +0700

    mint with whitelist

---
+++ migrations/environmentNFT/2.4_whitelistmint_nft.ts
@@ -0,0 +1,24 @@
+import {EnvironmentNFT} from "./environmentNFT";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "local") {
+            console.log("wrong network");
+            return;
+        }
+        
+        let nftContract: any;
+        nftContract = process.env.ENVIRONMENT_NFT_CONTRACT;
+        if (process.argv.length >= 2) {
+            nftContract = process.argv[2];
+        }
+        console.log("nftContract:", nftContract);
+
+        const nft = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        const tx = await nft.whitelistMint(nftContract, [1], 500000);
+        console.log(tx);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
