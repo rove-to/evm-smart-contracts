c9fac7ee424e5f6519301939c962d25b50dd216f
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-25 20:51:18 +0700

    - update

--- migrations/environmentNFT/environmentNFT.ts
+++ migrations/environmentNFT/environmentNFT.ts
@@ -107,13 +107,18 @@ class EnvironmentNFT {
         let temp = this.getContract(contractAddress);
         const nonce = await temp?.web3.eth.getTransactionCount(this.senderPublicKey, "latest") //get latest nonce
 
+        const fun = temp?.nftContract.methods.setCustomURI(tokenID, newTokenURI);
         //the transaction
         const tx = {
             from: this.senderPublicKey,
             to: contractAddress,
             nonce: nonce,
             gas: gas,
-            data: temp?.nftContract.methods.setCustomURI(tokenID, newTokenURI).encodeABI(),
+            data: fun.encodeABI(),
+        }
+
+        if (tx.gas == 0) {
+            tx.gas = await fun.estimateGas(tx);
         }
 
         return await this.signedAndSendTx(temp?.web3, tx);
