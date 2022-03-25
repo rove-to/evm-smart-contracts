a903a1a7988eab7af3c3a508d4ec2f43e65b0b12
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-25 09:29:37 +0700

    - update

--- contracts/utils/IERC1155Tradable.sol
+++ contracts/utils/IERC1155Tradable.sol
@@ -2,11 +2,14 @@
 pragma solidity 0.8.12;
 
 import "@openzeppelin/contracts/token/ERC1155/IERC1155.sol";
+import "@openzeppelin/contracts/interfaces/IERC2981.sol";
 
-interface IERC1155Tradable is IERC1155 {
-    function getCreator(uint256 id)
-    external
-    view
+interface IERC1155Tradable is IERC1155, IERC2981 {
+    function getCreator(uint256 id) external view
     virtual
     returns (address sender);
+
+    function royaltyInfo(uint256 _tokenId, uint256 _salePrice) external view
+    virtual
+    returns (address receiver, uint256 royaltyAmount);
 }
