d6bdc317270d3a047f2dcef3ba17548f30f9a36d
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-30 15:05:31 +0700

    - update

--- migrations/ticketNFT/1.1_deploy.ts
+++ migrations/ticketNFT/1.1_deploy.ts
@@ -4,13 +4,13 @@ import {TicketNFT} from "./ticketNFT";
 
 (async () => {
     try {
-        if (process.env.NETWORK != "local") {
+        if (process.env.NETWORK != "rinkeby") {
             console.log("wrong network");
             return;
         }
         const nft = new TicketNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await nft.deploy('', '', '');
-        console.log("%s EnvironmentNFT deployed address: %s", process.env.NETWORK, address);
+        const address = await nft.deploy(process.env.PUBLIC_KEY, process.env.PUBLIC_KEY, '0x87e3A224cc55569664A1097A0293B3d48d731299');
+        console.log("%s TicketNFT deployed address: %s", process.env.NETWORK, address);
     } catch (e) {
         // Deal with the fact the chain failed
         console.log(e);
