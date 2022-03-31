3e56ecdc7dd3830ee3df1b02c297fff79414a66e
thaibao-incognito <thaibao@autonomous.nyc>
2022-03-31 22:01:02 +0700

    - update

--- migrations/environmentNFT/1.1_deploy.ts
+++ migrations/environmentNFT/1.1_deploy.ts
@@ -9,7 +9,11 @@ import {EnvironmentNFT} from "./environmentNFT";
             return;
         }
         const nft = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const address = await nft.deploy('', '', 'Rove Environments', 'REs');
+        const address = await nft.deploy(
+            '',
+            '',
+            'Rove Environments',
+            'REs');
         console.log("%s EnvironmentNFT deployed address: %s", process.env.NETWORK, address);
     } catch (e) {
         // Deal with the fact the chain failed
