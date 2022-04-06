923b67c7b0dcb47c3b8f7db592ee54838c40d678
thaibao-incognito <thaibao@autonomous.nyc>
2022-04-06 15:11:44 +0700

    - set price tool

---
+++ migrations/environmentNFT/4_setprice.ts
@@ -0,0 +1,34 @@
+import {EnvironmentNFT} from "./environmentNFT";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "local") {
+            console.log("wrong network");
+            return;
+        }
+
+        let contract: any = process.env.ENVIRONMENT_NFT_CONTRACT;
+        if (process.argv.length >= 2) {
+            contract = process.argv[2];
+            console.log("contract", contract)
+        }
+
+        let tokenID: any;
+        if (process.argv.length >= 3) {
+            tokenID = process.argv[3];
+            console.log("tokenID", tokenID)
+        }
+
+        let price: any;
+        if (process.argv.length >= 4) {
+            price = process.argv[4];
+            console.log("tokenUri", price)
+        }
+
+        const nft = new EnvironmentNFT(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        await nft.changePriceToken(contract, tokenID, price, 500000);
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
