c31680a2ff676e151eda141843a2237de4478ebf
thaibao-incognito <thaibao@autonomous.nyc>
2022-04-29 14:06:51 +0700

    - optimize Rock nft

--- contracts/goods/RockNFT.sol
+++ contracts/goods/RockNFT.sol
@@ -15,7 +15,6 @@ import "hardhat/console.sol";
 
 contract RockNFT is ERC1155Tradable {
     event ParameterControlChanged (address previous, address new_);
-    event PreCreateEvent (address _initialOwner, uint256 _id, uint256 _initialSupply, string _uri, address _operator);
 
     mapping(uint256 => address) public metaverseOwners;
 
@@ -74,18 +73,14 @@ contract RockNFT is ERC1155Tradable {
     }
 
     function _prepareCreateNft(
-        address _initialOwner,
         uint256 _id,
-        bytes memory _data,
         uint256 _price
     ) internal returns (uint256) {
         require(!_exists(_id), "ALREADY_EXIST");
-        uint _supply = 1;
 
         price_tokens[_id] = _price;
         metaverseOwners[_id] = _msgSender();
 
-        emit PreCreateEvent(_initialOwner, _id, _supply, "", operator);
         return _id;
     }
 
@@ -100,8 +95,6 @@ contract RockNFT is ERC1155Tradable {
 
         if (price_tokens[_id] > 0) {
             require(msg.value >= price_tokens[_id] * _quantity, "MISS_PRICE");
-        } else {
-            require(_quantity <= 1, "MAX_QUANTITY");
         }
 
         creators[_id] = operator;
@@ -141,7 +134,7 @@ contract RockNFT is ERC1155Tradable {
         }
         for (uint256 i = initialRock; i < rockIds.length; i++) {
             console.log(i, gasleft());
-            _prepareCreateNft(recipient, rockIds[i], "0x", rockPrices[i]);
+            _prepareCreateNft(rockIds[i], rockPrices[i]);
         }
     }
 }
