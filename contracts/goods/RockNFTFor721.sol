036cdc298c7b96ca3a8395a47d054b53ff9c85f7
thaibao-incognito <thaibao@autonomous.nyc>
2022-05-06 12:18:59 +0700

    - update RockNFT

--- contracts/goods/RockNFTFor721.sol
+++ contracts/goods/RockNFTFor721.sol
@@ -14,15 +14,17 @@ import "../governance/ParameterControl.sol";
  *
  */
 
-contract RockNFT is ERC1155Tradable {
+contract RockNFTFor721 is ERC1155Tradable {
     event ParameterControlChanged (address previous, address new_);
 
     mapping(string => address) public metaverseOwners;
 
-    // for rock by public
-    mapping(string => uint256) public metaversePublicRocksSize;
-    mapping(string => uint256) public metaversePublicRockPrice;
+    // for rock base on erc-721
+    mapping(string => address) public metaverseNFTColl;
+    mapping(string => uint256) public metaverseNFTCollRocksSize;
+    mapping(string => uint256) public metaverseNFTCollRockPrice;
 
+    mapping(address => mapping(uint256 => bool)) minted;
 
     using SafeMath for uint256;
 
@@ -65,6 +67,18 @@ contract RockNFT is ERC1155Tradable {
 
     }
 
+    function sliceUint(bytes memory bs, uint start)
+    internal pure
+    returns (uint256)
+    {
+        require(bs.length >= start + 32, "OOR");
+        uint256 x;
+        assembly {
+            x := mload(add(bs, add(0x20, start)))
+        }
+        return x;
+    }
+
     function mintRock(
         string memory _metaverseId,
         address _to,
@@ -76,15 +90,32 @@ contract RockNFT is ERC1155Tradable {
         require(metaverseOwners[_metaverseId] != address(0x0), "N_EXI_M");
         require(!_exists(_id), "ALREADY_EXIST");
 
-        // rock as public: run when no rocks base on erc-721 or minted full rock base on erc-721 
-        require(metaversePublicRocksSize[_metaverseId] > 0, "OOS");
-        if (price_tokens[_id] > 0) {
-            require(msg.value >= price_tokens[_id], "MISS_PRI_P");
-        } else {
-            require(msg.value >= metaversePublicRockPrice[_metaverseId], "MISS_PRI_P");
-        }
+        address _erc721Add = metaverseNFTColl[_metaverseId];
+        require(_erc721Add != address(0x0), "MISS_ERC721_TOKEN");
+        if (_erc721Add != address(0x0) && metaverseNFTCollRocksSize[_metaverseId] > 0) {
+            require(_data.length > 0, "MISS_ERC721_TOKEN");
+            require(metaverseNFTCollRocksSize[_metaverseId] > 0, "OOS_");
+            /* check erc-721 */
+            ERC721 _erc721 = ERC721(_erc721Add);
+            // get token erc721 id from _data
+            uint256 _erc721Id = sliceUint(_data, 0);
+            // check owner token id
+            require(_erc721.ownerOf(_erc721Id) == msgSender(), "N_OWN_ERC721");
+            // check token not minted 
+            require(!minted[_erc721Add][_erc721Id], "MINTED");
 
-        metaversePublicRocksSize[_metaverseId]--;
+            // marked this erc721 token id is minted ticket
+            minted[_erc721Add][_erc721Id] = true;
+
+            if (price_tokens[_id] > 0) {
+                require(msg.value >= price_tokens[_id], "MISS_PRI_N");
+            } else {
+                require(msg.value >= metaverseNFTCollRockPrice[_metaverseId], "MISS_PRI_N");
+            }
+
+
+            metaverseNFTCollRocksSize[_metaverseId]--;
+        }
         creators[_id] = operator;
         if (bytes(_uri).length > 0) {
             customUri[_id] = _uri;
@@ -93,9 +124,9 @@ contract RockNFT is ERC1155Tradable {
         _mint(_to, _id, 1, _data);
 
         // check user mint fee
-        if (price_tokens[_id] > 0 || metaversePublicRockPrice[_metaverseId] > 0) {
+        if (price_tokens[_id] > 0 || metaverseNFTCollRockPrice[_metaverseId] > 0) {
             ParameterControl _p = ParameterControl(parameterControlAdd);
-            uint256 purchaseFeePercent = _p.getUInt256("ROCK_PUR_FEE");
+            uint256 purchaseFeePercent = _p.getUInt256("ROCK_721_PUR_FEE");
             uint256 fee = msg.value * purchaseFeePercent / 10000;
             (bool success,) = metaverseOwners[_metaverseId].call{value : msg.value - fee}("");
             require(success, "FAIL");
@@ -105,8 +136,9 @@ contract RockNFT is ERC1155Tradable {
     }
 
     function initMetaverse(string memory metaverseId,
-        uint256 pricePublic,
-        uint256 rockIdsPublicSize
+        address erc721Addr,
+        uint256 priceNftColl,
+        uint256 rockIdNFTCollsSize
     )
     external payable
     {
@@ -115,18 +147,24 @@ contract RockNFT is ERC1155Tradable {
         // get params
         ParameterControl _p = ParameterControl(parameterControlAdd);
         // get fee for imo
-        uint256 imoFEE = _p.getUInt256("INIT_IMO_FEE");
+        uint256 imoFEE = _p.getUInt256("INIT_721_IMO_FEE");
         if (imoFEE > 0) {
-            require(msg.value >= imoFEE * rockIdsPublicSize, "MISS_INI_FEE");
+            require(msg.value >= imoFEE * rockIdNFTCollsSize, "MISS_INI_FEE");
         }
 
-        // -- rock as public
-        if (rockIdsPublicSize > 0) {
-            require(pricePublic > 0, "MISS_PRI_P");
+        // metaverse owner
+        metaverseOwners[metaverseId] = msgSender();
+
+        // -- rock base on erc-721 nft collection
+        metaverseNFTColl[metaverseId] = erc721Addr;
+        if (erc721Addr != address(0x0)) {
+            require(rockIdNFTCollsSize > 0, "INV_COL");
             // set price
-            metaversePublicRockPrice[metaverseId] = pricePublic;
+            metaverseNFTCollRockPrice[metaverseId] = priceNftColl;
             // set rocks list
-            metaversePublicRocksSize[metaverseId] = rockIdsPublicSize;
+            metaverseNFTCollRocksSize[metaverseId] = rockIdNFTCollsSize;
+        } else {
+            require(rockIdNFTCollsSize == 0, "INV_COL");
         }
     }
 }
---
+++ migrations/rockNFT721/4._seturi.ts
@@ -0,0 +1,37 @@
+import {RockNFT721} from "./rockNFT";
+
+(async () => {
+    try {
+        if (process.env.NETWORK != "local") {
+            console.log("wrong network");
+            return;
+        }
+
+        let contract: any = process.env.ENVIRONMENT_NFT_CONTRACT;
+        if (process.argv.length >= 2) {
+            contract = process.argv[2];
+            console.log("contract", contract)
+        }
+
+        let tokenID: any;
+        if (process.argv.length >= 3) {
+            tokenID = process.argv[3];
+            console.log("tokenID", tokenID)
+        }
+
+        let tokenUri: any = "";
+        if (process.argv.length >= 4) {
+            tokenUri = process.argv[4];
+            console.log("tokenUri", tokenUri)
+        }
+
+        const nft = new RockNFT721(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
+        if (tokenUri.length > 0) {
+            await nft.setCustomTokenUri(contract, tokenID, tokenUri, 0);
+        }
+        console.log("token uri", await nft.uri(contract, tokenID));
+    } catch (e) {
+        // Deal with the fact the chain failed
+        console.log(e);
+    }
+})();
