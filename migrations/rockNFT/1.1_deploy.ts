16aab8f034d58b4489dea4d86590799b7b4d21f7
thaibao-incognito <thaibao@autonomous.nyc>
2022-05-17 10:18:18 +0700

    - update

--- migrations/rockNFT/1.1_deploy.ts
+++ migrations/rockNFT/1.1_deploy.ts
@@ -13,8 +13,8 @@ import {RockNFT} from "./rockNFT";
             '0xE55EAdE1B17BbA28A80a71633aF8C15Dc2D556A5',
             '0xE55EAdE1B17BbA28A80a71633aF8C15Dc2D556A5',
             '0x87e3A224cc55569664A1097A0293B3d48d731299',
-            "Rove Rocks",
-            "RRs");
+            "Metaverse powered by Rove",
+            "RMs");
         console.log("%s RockNFT deployed address: %s", process.env.NETWORK, address);
     } catch (e) {
         // Deal with the fact the chain failed
