72f47ad94d26c528c9adc169014ede5ddc110907
thaibao-incognito <thaibao@autonomous.nyc>
2022-06-17 10:47:42 +0700

    - update

--- migrations/rockNFT/1.1_deploy2.ts
+++ migrations/rockNFT/1.1_deploy2.ts
@@ -4,7 +4,7 @@ import {RockNFT} from "./rockNFT";
 
 (async () => {
     try {
-        if (process.env.NETWORK != "mumbai") {
+        if (process.env.NETWORK != "local") {
             console.log("wrong network");
             return;
         }
@@ -12,7 +12,7 @@ import {RockNFT} from "./rockNFT";
         const address = await nft.deploy2(
             '0xE55EAdE1B17BbA28A80a71633aF8C15Dc2D556A5',
             '0xF61234046A18b07Bf1486823369B22eFd2C4507F',
-            '0xF61234046A18b07Bf1486823369B22eFd2C4507F',
+            '0xae0d1e38B696ee0Ac5Ce6B9b746aa793e1485a9e',
             '0xFD8500cf6B98F37Bc1a287195d2537b72945a1e8',
             "Metaverse powered by Rove",
             "RMs",
