16cb828d40e37a8d3e183559fa1861f1b53e4811
thaibao-incognito <thaibao@autonomous.nyc>
2022-07-12 13:52:14 +0700

    - update

---
+++ migrations/environmentNFT/test.ts
@@ -0,0 +1,285 @@
+const info = [
+    {
+        rockId: '628dcfd76f0d2bc7f2a55e2f',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f53',
+        userId: '629e5475a9468fc8666f53ac',
+        email: 'botinochko@gmail.com',
+        name: 'botinochko',
+        address: '0xb18b2806ad299aeee39099c357ead3ef2c98ce7d',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f59',
+        userId: '61f0bbc8c7251c1c5b1f75d1',
+        email: 'we@rove.to',
+        name: 'Rove-Team',
+        address: '0xf287d98a7b0823a49cfd20e250a251b97561c6ad',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f42',
+        userId: '629df024917fd87e88c907b4',
+        email: 'mikolas666111@gmail.com',
+        name: '2022',
+        address: '0xa6416899edaa5392239eec747d9638d348e47ced',
+        type: 0
+    },
+    {
+        rockId: '628dcfd76f0d2bc7f2a55e3c',
+        userId: '629df024917fd87e88c907b4',
+        email: 'mikolas666111@gmail.com',
+        name: '2022',
+        address: '0xa6416899edaa5392239eec747d9638d348e47ced',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f60',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfd76f0d2bc7f2a55e3b',
+        userId: '629df024917fd87e88c907b4',
+        email: 'mikolas666111@gmail.com',
+        name: '2022',
+        address: '0xa6416899edaa5392239eec747d9638d348e47ced',
+        type: 0
+    },
+    {
+        rockId: '628dcfd96f0d2bc7f2a55e93',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f4c',
+        userId: '6215a39d2a8e31a8db800dd7',
+        email: 'giabao@autonomous.nyc',
+        name: 'aliasalias',
+        address: '0x4f39a4f47afab857f194157ebbaf5c6d1815e362',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f3d',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f3c',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f76',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f81',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f75',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f71',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f6f',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f69',
+        userId: '629475d59f7ac7c2d3852b06',
+        email: 'alexandru_razvan_s@yahoo.com',
+        name: 'alexmaxx',
+        address: '0x12c23132185ee6329c3b1852ba7d868e510313c5',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f84',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f65',
+        userId: '61f0bbc8c7251c1c5b1f75d1',
+        email: 'we@rove.to',
+        name: 'Rove-Team',
+        address: '0xf287d98a7b0823a49cfd20e250a251b97561c6ad',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f6c',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f70',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f62',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f90',
+        userId: '61f20d3ab3ea84e16446d2a6',
+        email: 'duonghongsg@gmail.com',
+        name: 'duonghongsg',
+        address: '0x0ce19734be7dc49d2f2e8bfd6ad99cf583eff601',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f95',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f79',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f77',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f9f',
+        userId: '61f0bbc8c7251c1c5b1f75d1',
+        email: 'we@rove.to',
+        name: 'Rove-Team',
+        address: '0xf287d98a7b0823a49cfd20e250a251b97561c6ad',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f91',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f88',
+        userId: '62a23bbe928b59a126319fa8',
+        email: 'randomawesomemail99@gmail.com',
+        name: 'randomawesomemail99',
+        address: '0xfffd3b00a9faf394f3d8b81e5cbfe0b6e8d91dbf',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f8c',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f94',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f92',
+        userId: '61f20d3ab3ea84e16446d2a6',
+        email: 'duonghongsg@gmail.com',
+        name: 'duonghongsg',
+        address: '0x0ce19734be7dc49d2f2e8bfd6ad99cf583eff601',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f73',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    },
+    {
+        rockId: '628dcfdb6f0d2bc7f2a55f7a',
+        userId: '62614edba8895aed6c91395f',
+        email: 'ascoluofficial@gmail.com',
+        name: 'Ascolu',
+        address: '0x5a70254571742d304d61a4154b1c307f91988205',
+        type: 0
+    }
+];
+
+(async () => {
+    let accounts: any = {};
+    info.forEach((value, index) => {
+        if (value.name == 'duonghongsg' || value.name == 'Rove-Team') {
+            return;
+        }
+        accounts[value.userId] = {name: value.name, address: value.address};
+    });
+    console.log(accounts);
+})();
