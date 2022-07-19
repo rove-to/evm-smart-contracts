b18e2432d9253c566d729751c9042a0164e7f829
thaibao-incognito <thaibao@autonomous.nyc>
2022-07-19 15:35:01 +0700

    - update

--- migrations/erc721tradeable/2.2_minttomulti.ts
+++ migrations/erc721tradeable/2.2_minttomulti.ts
@@ -8,16 +8,17 @@ import {Erc721} from "./erc721";
         }
 
         const marketplace = new Erc721(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
-        const current = await marketplace.nextTokenId("");
+        const contractAddress = '0xae0C96BBD7733a1C7843af27e0683c74E182A3a7';
+        const current = await marketplace.nextTokenId(contractAddress);
         const fromTokenId = current + 1;
-        const x = -1;
-        const toTokenId = fromTokenId + x;
+        const x = 10;
+        const toTokenId = fromTokenId - 1 + x;
         for (var i = fromTokenId; i <= toTokenId; i++) {
             const tx = await marketplace.mintTo(
-                "",
-                "",
+                contractAddress,
+                "0xEB39aFAdcf1CBC8266fa0f4c0F35BEeaE556C920",
                 "", 0);
-            console.log("Tx success on : ", tx?.transactionHash);
+            console.log(i + ". Tx success on: ", tx?.transactionHash);
         }
     } catch (e) {
         // Deal with the fact the chain failed
