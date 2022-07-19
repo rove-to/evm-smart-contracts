e0602f0eaba2167c710f7ccddea3e84a5e7b0511
thaibao-incognito <thaibao@autonomous.nyc>
2022-07-19 15:35:26 +0700

    update

--- migrations/erc721tradeable/1.2_deployupgradeable.ts
+++ migrations/erc721tradeable/1.2_deployupgradeable.ts
@@ -9,11 +9,11 @@ import {Erc721} from "./erc721";
 
         const marketplace = new Erc721(process.env.NETWORK, process.env.PRIVATE_KEY, process.env.PUBLIC_KEY);
         const address = await marketplace.deployUpgradeable(
-            "",
-            "",
-            "",
-            "",
-            "");
+            "AUTOREX",
+            "ATR",
+            "https://api.autonomous.ai/nft/metadata/",
+            "0xE55EAdE1B17BbA28A80a71633aF8C15Dc2D556A5",
+            "0xF61234046A18b07Bf1486823369B22eFd2C4507F");
         console.log("Autonomous ERC721 deployed address: ", address);
     } catch (e) {
         // Deal with the fact the chain failed
